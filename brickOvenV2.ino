af837b5e4e2b78e34b599ec3b6c8a51b7bec1a81
JohnAbbott <jabbott@abbotts.org>
2023-07-06 11:17:10 -0500

    Update brickOvenV2.ino
    Finalized Setup section

--- brickOvenV2.ino
+++ brickOvenV2.ino
@@ -11,9 +11,13 @@
 
 
 #include "WiFiS3.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include "Adafruit_MAX31855.h"
 
 #include "pussy_secrets.h" 
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+///////Use the info in tab/arduino_secrets.h to login to the network Pussy_Galore
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0; 
@@ -22,38 +26,97 @@ int status = WL_IDLE_STATUS;
 
 WiFiServer server(80);
 
+#define MAXDO   8
+#define MAXCS   10
+#define MAXCLK  9
+
+LiquidCrystal_I2C lcd(0x3F,20,4);
+LiquidCrystal_I2C lcd2(0x27,20,4);
+// A5 (SCL) and A4 (SDA).
+
+// initialize the Thermocouple
+Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
+  //DO connects to digital 8, CS connects to digital 10, and CLK connects to pin 9
+//Mux control pins
+int s0 = 4;
+int s1 = 5;
+int s2 = 6;
+int s3 = 7;
+
+int currentTemp;  // this holds the temp gotten in the thermocouple.readFahrenheit()
+
 void setup() {
-  //Initialize serial and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
+  /*
+   *  WI-FI Setup
+   *    This whole block sets up the web server on the Arduino R4
+   */
+   //Initialize serial and wait for port to open:
+   Serial.begin(9600);
+   Serial.print("Initializing sensor...");
+   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
-  }
+   }
 
-  // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    // don't continue
-    while (true);
-  }
+   // check for the WiFi module:
+   if (WiFi.status() == WL_NO_MODULE) {
+     Serial.println("Communication with WiFi module failed!");
+     // don't continue
+     while (true);
+   }
 
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
+   String fv = WiFi.firmwareVersion();
+   if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+     Serial.println("Please upgrade the firmware");
+   }
 
-  // attempt to connect to WiFi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
+   // attempt to connect to WiFi network:
+   while (status != WL_CONNECTED) {
+     Serial.print("Attempting to connect to SSID: ");
+     Serial.println(ssid);
+     // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+     status = WiFi.begin(ssid, pass);
 
-    // wait 10 seconds for connection:
-    delay(10000);
-  }
-  server.begin();
-  // you're connected now, so print out the status:
-  printWifiStatus();
+     // wait 10 seconds for connection:
+     delay(10000);
+    }
+   server.begin();
+   // you're connected now, so print out the status:
+   printWifiStatus();
+
+  /*
+   * This is the initialization of the Multiplexer
+   */
+   Serial.print("Initializing MUX...");
+   pinMode(s0, OUTPUT); 
+   pinMode(s1, OUTPUT); 
+   pinMode(s2, OUTPUT); 
+   pinMode(s3, OUTPUT); 
+
+   digitalWrite(s0, LOW);
+   digitalWrite(s1, LOW);
+   digitalWrite(s2, LOW);
+   digitalWrite(s3, LOW);
+  
+  /*
+   * Initalization of the LCD displays
+   */
+   Serial.print("Initializing LCD");
+   lcd.init();                      // initialize the lcd 
+   lcd.init();
+   lcd.backlight();
+   // The second display
+   lcd2.init();                      // initialize the lcd 
+   lcd2.init();
+   lcd2.backlight();
+
+  /*
+   * Intialize the Thermocouple Amplifier
+   */
+   Serial.print("Initializing Thermocouple Amplifier");
+   if (!thermocouple.begin()) {
+     Serial.println("ERROR.");
+     while (1) delay(10);
+   }
 }
 
 
